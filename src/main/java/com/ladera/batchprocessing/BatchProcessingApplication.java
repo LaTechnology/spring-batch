9fb1c4fe96a5d88d22b55db953d520efeed28d23
RahulS23071998 <rahul.s@laderatechnology.com>
2023-12-29 12:33:24 +0530

    Added code for java

---
+++ src/main/java/com/ladera/batchprocessing/BatchProcessingApplication.java
@@ -0,0 +1,14 @@
+package com.ladera.batchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.ladera.batchprocessing")
+public class BatchProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProcessingApplication.class, args);
+	}
+
+}
